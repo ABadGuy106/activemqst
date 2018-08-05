0bf56de60961c8c87ff8e6a208eea11e33f858ed
liujiwei@csii.com.cn <LIUjiwei106+>
2018-08-05 10:45:18 +0800

    Active基本使用方式

--- activemq01/src/main/java/net/abadguy/hello/lisenter/JMSConsumerLisenter.java
+++ activemq01/src/main/java/net/abadguy/hello/lisenter/JMSConsumerLisenter.java
@@ -14,44 +14,30 @@ import javax.jms.*;
 
 public class JMSConsumerLisenter {
 
-    private static final String USERNAME= ActiveMQConnection.DEFAULT_USER;//默认连接用户名
-    private static final String PASSWORD = ActiveMQConnection.DEFAULT_PASSWORD;
-    private static final String BROKEURL=ActiveMQConnection.DEFAULT_BROKER_URL;
+    private static final String USERNAME=ActiveMQConnection.DEFAULT_USER; // 默认的连接用户名
+    private static final String PASSWORD=ActiveMQConnection.DEFAULT_PASSWORD; // 默认的连接密码
+    private static final String BROKEURL=ActiveMQConnection.DEFAULT_BROKER_URL; // 默认的连接地址
 
-    public static void main(String[] args){
-        ConnectionFactory connectionFactory;//连接工厂
-        Connection connection = null;//连接
-        Session session;//会话 接收或者发送消息的线程
-        Destination destination; //消息的目的地
-        MessageConsumer consumer;//消息消费者
+    public static void main(String[] args) {
+        ConnectionFactory connectionFactory; // 连接工厂
+        Connection connection = null; // 连接
+        Session session; // 会话 接受或者发送消息的线程
+        Destination destination; // 消息的目的地
+        MessageConsumer messageConsumer; // 消息的消费者
 
-        //实例化连接工厂
-        connectionFactory=new ActiveMQConnectionFactory(USERNAME,PASSWORD,BROKEURL);
+        // 实例化连接工厂
+        connectionFactory=new ActiveMQConnectionFactory(USERNAME, PASSWORD, BROKEURL);
 
         try {
-            connection=connectionFactory.createConnection();
-
-
-            connection.start();
-
-            session=connection.createSession(Boolean.FALSE,Session.AUTO_ACKNOWLEDGE);
-            destination=session.createQueue("FirstQue1");
-            consumer=session.createConsumer(destination);
-            consumer.setMessageListener(new Lisenter());//注册消息监听
-
-
-
+            connection=connectionFactory.createConnection();  // 通过连接工厂获取连接
+            connection.start(); // 启动连接
+            session=connection.createSession(Boolean.FALSE, Session.AUTO_ACKNOWLEDGE); // 创建Session
+            destination=session.createQueue("FirstQue1");  // 创建连接的消息队列
+            messageConsumer=session.createConsumer(destination); // 创建消息消费者
+            messageConsumer.setMessageListener(new Lisenter()); // 注册消息监听
         } catch (JMSException e) {
+            // TODO Auto-generated catch block
             e.printStackTrace();
-        }finally {
-            if(connection!=null){
-                try {
-                    connection.close();
-                } catch (JMSException e) {
-                    e.printStackTrace();
-                }
-            }
         }
-
     }
 }
